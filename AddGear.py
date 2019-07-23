a1433a7ec38ddd82bc3bc814e7e6806a88626ae8
clintsearl <clintsearl@gmail.com>
2019-07-23 10:40:54 -0600

    filling out the half of the form

--- AddGear.py
+++ AddGear.py
@@ -1,16 +1,26 @@
 print("hello World")
 from selenium import webdriver
-
+from selenium.webdriver.support.ui import Select
 driver = webdriver.Chrome()
 loadpage = driver.get("https://hiking-gear.herokuapp.com/addgear")
-name = loadpage.find_element_by_name("name")
-
-
-name.send_keys("Rain Coat")
-
-brand = loadpage.find_element_by_name("brand")
-brand.send_keys("REI")
+name = driver.find_element_by_name("name")
 
 driver.implicitly_wait(5)
 
+name.click()
+name.send_keys("Rain Coat")
+
+brand = driver.find_element_by_name("brand")
+brand.send_keys("REI")
+
+catagory = driver.find_element_by_tag_name("select")
+catagory.click()
+# catagory.find_element("option")
+# allOptions = catagory.find_elements_by_tag_name("option")
+# for option in allOptions:
+    # print option.get_attribute("value")
+choose = Select(driver.find_element_by_tag_name("select"))
+choose.select_by_visible_text("Clothing")
+
+units = driver.find
 driver.quit()
